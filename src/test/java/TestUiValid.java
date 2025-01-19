133aa178606acd79c3ddd5d527bc29219de3ef48
katana <klimenko.katrin888@mail.ru>
2025-01-19 17:07:01 +0300

    test

--- src/test/java/TestUiValid.java
+++ src/test/java/TestUiValid.java
@@ -22,11 +22,10 @@ public class TestUiValid {
     @BeforeEach
     void setUp() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
